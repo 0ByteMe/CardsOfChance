6b84a5eea22f0937a09bf9353819262ed79634ff
SamWhoLearns <samwholearns@gmail.com>
2022-04-28 16:15:38 -0500

    Added Main Menu Screen
    Added transitions to play game and exit application

---
+++ Card Game/Assets/ManageScenes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageScenes : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+}
